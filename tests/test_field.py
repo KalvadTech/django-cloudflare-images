9e77639d1d38b643fcedac30c258c3173f4afa58
Pierre Guillemot <pierre@kalvad.com>
2022-03-25 11:34:45 +0400

    more tests

--- tests/test_field.py
+++ tests/test_field.py
@@ -4,13 +4,12 @@ Tests related to the CloudflareImagesField
 
 from django.test import TestCase
 from django.conf import settings
-from cloudflare_images.field import CloudflareImagesField
+from cloudflare_images.field import CloudflareImagesField, CloudflareImagesFieldFile
 
 
 class CloudflareImageFieldTests(TestCase):
     """
     Test case for the CloudflareImagesField
-    TODO: add tests for CloudflareImagesFieldFile for url method
     """
 
     def test_default_variant(self):
@@ -21,3 +20,23 @@ class CloudflareImageFieldTests(TestCase):
         variant = "custom"
         field = CloudflareImagesField(variant=variant)
         self.assertEqual(field.variant, variant)
+
+
+class CloudflareImageFieldFileTests(TestCase):
+    """
+    Test case for the CloudflareImagesFieldFile
+    """
+
+    def test_url_default_variant(self):
+        field = CloudflareImagesField()
+        field_file = CloudflareImagesFieldFile(None, field, "image_id")
+        url = field_file.url
+        hardcoded_url = "https://imagedelivery.net/account_hash/image_id/public"
+        self.assertEqual(url, hardcoded_url)
+
+    def test_url_custom_variant(self):
+        field = CloudflareImagesField(variant="custom")
+        field_file = CloudflareImagesFieldFile(None, field, "image_id")
+        url = field_file.url
+        hardcoded_url = "https://imagedelivery.net/account_hash/image_id/custom"
+        self.assertEqual(url, hardcoded_url)
