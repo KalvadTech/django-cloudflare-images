6d41c5d4d4f6e2eb75006883263090e00d4b42d9
Pierre Guillemot <pierre@kalvad.com>
2022-06-28 15:33:34 +0400

    code format

--- tests/settings.py
+++ tests/settings.py
@@ -6,7 +6,7 @@ DATABASES = {
     }
 }
 SECRET_KEY = "top_secret"
-USE_TZ = True # This is here to avoid Django's warning when running tests about the upcoming change in Django 5.0
+USE_TZ = True  # This is here to avoid Django's warning when running tests about the upcoming change in Django 5.0
 
 DEFAULT_FILE_STORAGE = "cloudflare_images.storage.CloudflareImagesStorage"
 CLOUDFLARE_IMAGES_ACCOUNT_ID = "account_id"
