899d65048ab2abf4b80dd407f3b14db8391aaeb9
pierre <pierre@kalvad.com>
2025-11-27 14:25:00 +0400

    display current image

--- cloudflare_images/widget.py
+++ cloudflare_images/widget.py
@@ -3,6 +3,7 @@ Custom widget for Direct Creator Uploads
 """
 
 from django.forms import ClearableFileInput
+from cloudflare_images.service import CloudflareImagesService
 
 
 class CloudflareImagesWidget(ClearableFileInput):
@@ -17,7 +18,10 @@ class CloudflareImagesWidget(ClearableFileInput):
 
     def get_context(self, name, value, attrs):
         context = super().get_context(name, value, attrs)
+        service = CloudflareImagesService()
+
         context["widget"]["value"] = value
+        context["widget"]["url"] = service.get_url(value, "public") if value else None
         return context
 
     class Media:
